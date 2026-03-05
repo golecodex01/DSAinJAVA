358fb9d62a2a94b83eecf278d9c8de0439953dcc
golecodex <mohitgole999@gmail.com>
2026-03-06 00:04:04 +0530

    Solved Q283: Move Zeroes using Two Pointer Approach Time Complexity: O(n) Space Complexity: O(1)
    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
    
    Note that you must do this in-place without making a copy of the array.

---
+++ Arrays/Q283_Move_Zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonzeroelement=0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]!=0){
+                      int temp=nums[i];
+                      nums[i]=nums[nonzeroelement];
+                      nums[nonzeroelement]=temp;
+                      nonzeroelement++;
+
+
+            }
+        }
+        
+    }
+}
